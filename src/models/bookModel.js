9cd6cabaadac2b00e6b80453c4e3231255d346fb
archanaharishikm <archanaharish@ikm.gov.in>
2026-02-24 16:30:00 +0530

    Replace UUID generation with crypto's randomUUID for improved uniqueness

--- src/models/bookModel.js
+++ src/models/bookModel.js
@@ -1,10 +1,10 @@
 const mongoose = require('mongoose');
-const { v4: uuidv4 } = require('uuid');
+const { randomUUID } = require('crypto');
 
 const bookSchema = new mongoose.Schema({
      _id: {
         type: String,
-        default: uuidv4
+        default: () => randomUUID()
      },
     title: {
         type: String,
